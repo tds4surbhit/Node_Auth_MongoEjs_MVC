372707de39cab05f7b68c77bd170bd6978882c41
tds4surbhit <surbhit.4zeracing@gmail.com>
2022-10-23 04:31:28 +0530

    Adding Routes , Controller and View like MVC Patterns

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb://localhost/Smoothie-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes)
