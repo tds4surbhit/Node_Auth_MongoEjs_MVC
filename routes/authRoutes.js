56db21104f7ef57c3a73b4f4b0d9bf89205afe3b
tds4surbhit <surbhit.4zeracing@gmail.com>
2022-11-08 16:53:15 +0530

    Adding a new Api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,5 @@ router.get('/signup',authController.signup_get);
 router.post('/signup',authController.signup_post);
 router.get('/login',authController.login_get);
 router.post('/login',authController.login_post);
-
+router.get('/details',authController.getDetail);
 module.exports = router;
